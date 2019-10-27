547714920d2acad07d0c059e001682e7ca2d3df3
Khoi <nkason333@gmail.com>
2019-10-27 14:23:55 -0700

    javascript

--- javascript activity/Unsolved/02_More_Variables/unsolved/js/index.js
+++ javascript activity/Unsolved/02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,17 @@
 // Create the addNumbers function here.
+function addNumber() {
+  var two = 2;
+  var three = 3;
+  var five = two + three;
+  console.log(five);
+}
+addNumber(); // Dont put this into function addNumber because 'addNumber' is already inside function
 
 // Create the stringConcat function here.
+function stringConcat() {
+  var taco = "taco";
+  var cat = "cat";
+  var tacoCat = "taco" + "cat";
+}
+alert("tacoCat");
+springConcat();
