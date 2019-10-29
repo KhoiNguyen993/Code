d0af6f0879a7728e210398902dad1a3bc585964a
Khoi <nkason333@gmail.com>
2019-10-29 00:29:05 -0700

    nav updated

--- javascript19.2/unsolved/03_todo_app/js/index.js
+++ javascript19.2/unsolved/03_todo_app/js/index.js
@@ -1,5 +1,13 @@
 // Create event listener to enter items below
+$("#enter").on("click", function() {
+  var task = $("#todoItem").val();
+  $("#todoList").append(
+    "<div class='task'>" + task + "<div class='x fas fatimes'></div></div>"
+  );
+  $("#todoItem").val("");
+  console.log(task);
+});
 
 // Create event listener to delete items below
 
-// Create event listener to toggle 
+// Create event listener to toggle
